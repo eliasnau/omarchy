0e796df6fb0bc380c26c186b591b9d2722c9f80b
Elias Nau <contact@eliasnau.dev>
2026-03-28 20:10:48 +0100

    feat: make it easier to customize plymouth images by adding a config route

--- install/login/plymouth.sh
+++ install/login/plymouth.sh
@@ -1,4 +1,10 @@
 if [[ $(plymouth-set-default-theme) != "omarchy" ]]; then
   sudo cp -r "$HOME/.local/share/omarchy/default/plymouth" /usr/share/plymouth/themes/omarchy/
+
+  # Override with user customizations (e.g. custom logo.png or lock.png)
+  if compgen -G "$HOME/.local/share/omarchy/config/plymouth/*.png" &>/dev/null; then
+    sudo cp "$HOME/.local/share/omarchy/config/plymouth/"*.png /usr/share/plymouth/themes/omarchy/
+  fi
+
   sudo plymouth-set-default-theme omarchy
 fi
